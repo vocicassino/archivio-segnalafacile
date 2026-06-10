d1c4f70a31b42b5b0fb740e736d2be0c19dcd656
vocicassino <212131763+vocicassino@users.noreply.github.com>
2026-06-10 10:42:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "segnalafacile-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.mode === "navigate" || req.destination === "document") {
+    event.respondWith(fetch(req).catch(() => caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(req).then((cached) => cached || fetch(req).catch(() => cached)));
+});
